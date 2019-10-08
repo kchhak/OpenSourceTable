a9442c8cce21f5b5b68e6068ea214fd53d1c422d
Kaneka <kane.chhak@gmail.com>
2019-10-08 15:42:10 -0700

    Adds seed for guest login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+User.create(first_name: 'Guest', last_name: 'User', email: 'guest@email.com', password: 'guestpass', location: 'Charlotte', owner: true)
