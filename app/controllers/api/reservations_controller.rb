9c1c216ecae5d3f75b74bb23cdcd4fc2e3be0633
Kaneka <kane.chhak@gmail.com>
2019-10-14 10:13:38 -0700

    Routes and views for reservations

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,5 +1,42 @@
 class Api::ReservationsController < ApplicationController
-  def show
+  def show 
+    @reservation = Reservation.find(params[:id])
+  end
 
+  def index 
+    @user = User.find(params[:userId])
+
+    if @user 
+      @reservations = @user.reservations
+    else
+      render json: ["User not found"], status: 404
+    end
+  end
+
+  def create 
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save 
+      render :show
+    else
+      render json: @reservation.errors.full_messages, status: 422
+    end
+  end
+
+  def update 
+    @reservation = Reservation.find(params[:userId])
+
+    if @reservation.update(reservation_params)
+      render :show
+    else
+      render json: @reservation.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :restaurant_id, :num_guests, :res_time)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create]
+    resources :users, only: [:create] do 
+      resources :reservations, only: [:index]
+    end
     resource :session, only: [:create, :destroy]
     resources :restaurants, only: [:index, :show, :create, :update]
+    resources :reservations, only: [:show, :update, :create, :destroy]
   end
 end
