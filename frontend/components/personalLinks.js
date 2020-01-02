2555d7254f507d61a78a2e5b51f27075fe6012cd
Kaneka <kane.chhak@gmail.com>
2020-01-01 17:42:53 -0800

    Format personal links

--- frontend/components/personalLinks.js
+++ frontend/components/personalLinks.js
@@ -1,16 +1,25 @@
 import React from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faAngellist } from '@fortawesome/free-brands-svg-icons';
 
 const PersonalLinks = () => (
   <div className="footerlinks">
-    <p>Kaneka Chhak</p>
-    < a href="https://github.com/kchhak" >
-      <FontAwesomeIcon icon={faGithub} />
-    </a >
-    <a href="https://www.linkedin.com/in/kanekachhak/">
-      <FontAwesomeIcon icon={faLinkedin} />
-    </a>
+    <p>
+      Kaneka Chhak | software engineer currently seeking opportunities in
+      frontend, server side, or end to end development
+    </p>
+    <div className="links">
+      <a href="https://github.com/kchhak">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a href="https://www.linkedin.com/in/kanekachhak/">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a href="https://angel.co/kaneka-chhak/">
+        <FontAwesomeIcon icon={faAngellist} />
+      </a>
+    </div>
   </div>
 );
   
